64f221ae01d34330e181b6238af9a44aac4218a7
Lucca Zutin <luccazutin@gmail.com>
2022-04-12 06:37:56 +0100

    procfile added

--- run.py
+++ run.py
@@ -2,5 +2,6 @@ from api import app
 import os
 
 if __name__ == "__main__":
+    print(os.environ.get('PORT', 5000) +'---'*50)
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
+    app.run(debug=True, port=3000)
